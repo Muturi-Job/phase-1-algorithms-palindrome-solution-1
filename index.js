5a7664808dace79ce16147298bb6f0c5ac835e16
Job Muturi <muirimuturi17@gmail.com>
2023-03-29 13:23:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord = word.split("").reverse().join("");
+  return word === reverseWord;
 }
 
 /* 
   Add your pseudocode here
+  Converts the word to an array, reverses it and joins it back to a string
+  Returns true if the word is equal to the reversed word
 */
 
 /*
   Add written explanation of your solution here
+  reverse word converts the word to an array, splits it and joins it back to a string
+  then if the word and the reverse word are the same, it returns true. 
 */
 
 // You can run `node index.js` to view these console logs
